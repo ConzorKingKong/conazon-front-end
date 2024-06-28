72da2eca937317a855480ca691d09d49d8993e23
connor <connor@connors-MacBook-Pro.local>
2024-06-28 02:39:38 -0500

    update cart css.

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import './Cart.css'
 
 function Cart(props) {
   const {cart} = props
 
   return (
-    <div>Cart {JSON.stringify(cart)}</div>
+    <div className={"cart"} >
+      <p>Current Cart:</p>
+      <div>
+        {cart && cart[0] && cart.map((item, i) => {
+          return (
+            <div key={i} >
+              <p>{item.id}</p>
+              <p>{item.quantity}</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
