c0d2eba1eb82eaefc0a2111d330fa334e2487608
connor <connor@connors-MacBook-Pro.local>
2024-06-28 02:39:03 -0500

    Update about css.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './About.css'
 
 function About () {
   return (
-    <div>
-      Conazon is a project I made to test my Docker, Kubernetes, RabbitMQ, Golang, and React skills. Still a WIP.
+    <div className='about'>
+      <h1>About</h1>
+      <p>Conazon is a project I made to test my Docker, Kubernetes, RabbitMQ, Golang, and React skills. Still a WIP.</p>
     </div>
   )
 }
