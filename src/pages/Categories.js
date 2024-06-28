7e5a627fe42a6a9c56f94f68b064e95e2f219e12
connor <connor@connors-MacBook-Pro.local>
2024-06-28 02:40:03 -0500

    Add categories page.

---
+++ src/pages/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Categories.css'
+
+function Categories() {
+  return (
+    <div className='categories'>
+      <h1>Categories</h1>
+    </div>
+  )
+}
+
+export default Categories;
