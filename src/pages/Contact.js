d2163bdee5b127e97ef8a45c52a10464c9844bea
connor <connor@connors-MacBook-Pro.local>
2024-06-28 02:41:09 -0500

    Add contact page.

---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Contact.css'
+
+function Contact() {
+  return (
+    <div className={"contact"}>
+      <h1>Contact Me</h1>
+      <p>connor@connorpeshek.me</p>
+    </div>
+  )
+}
+
+export default Contact;
